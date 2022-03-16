c211b68dae061e3b9fed37c3946ea27beec3ff8d
ohad-porat <ohadp23@gmail.com>
2022-03-16 15:46:10 -0400

    import model from index.js

--- server/src/routes/api/v1/boardgamesRouter.js
+++ server/src/routes/api/v1/boardgamesRouter.js
@@ -1,6 +1,6 @@
 import express from "express"
 
-import BoardGame from "../../../models/BoardGame.js"
+import BoardGame from "../../../models/index.js"
 
 const boardGamesRouter = new express.Router()
 
